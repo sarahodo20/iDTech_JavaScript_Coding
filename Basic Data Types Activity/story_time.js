bd9962a2f6ce8dc08f516d96afdbb91e0ce300c6
sarahodo20 <modsod1012@gmail.com>
2024-07-03 11:53:43 -0400

    new file

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,30 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let greetings="Welcome to the wonderful world of pets!!"
+let name1="Ranger the Brave"
+let title="The Saga of Greenies"
+let place="Backyard Battlefield"
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age=1
+let year=2024
+let century=21
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weapons=["nails","teeth","jump", "kick", "tackle"]
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let enemyOrNot=true
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log(greetings+ " My name is " + name1 + ".")
+console.log ("Today I am going to tell you about " + title + " which happened in the " + place + ".")
+console.log("I am only " + age + " year old, but my bravery has caused me to be named the leader of my kingdom.")
+console.log("Currently, it is " + year + " which is the " + century + "st century.")
+console.log("The weapon I used to win the " + title + " was my amazing " + weapons[4] + ".")
+console.log("Many people ask me, 'Is Bingley the Loud really your enemy?' That is absolutely " + enemyOrNot + "! Bingley's bark is so annoying and he always tries to steal my greenies, as you will see in the story.")
+console.log("Anyway, now I will start to tell my tale...")
